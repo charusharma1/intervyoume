528027726c4e2f025cda69418e73d1032b7f6130
ACLTC <acltc@ACLTCs-MacBook-Air-8.local>
2015-12-13 16:55:17 -0800

    intervyoume-app theme upload

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,25 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require theme/jquery_11.1
+//= require theme/jquery_ui_1.11.1
+//= require theme/google_maps_api
+//= require theme/bootstrap.min
+//= require theme/bootstrap-hover-dropdown.min
+//= require theme/SmoothScroll
+//= require theme/jquery.dragtable
+//= require theme/jquery.card
+//= require theme/owl.carousel.min
+//= require theme/twitterFetcher_min
+//= require theme/color-switcher
+//= require theme/custom
+//= require_tree .
